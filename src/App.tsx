918c82664ece3af6c4c871c951f22f5691e03a6a
takumu <takumu155695@gmail.com>
2024-11-16 15:47:08 +0900

    メインビジュアルを設置

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,16 @@ import { headerLogo } from './data/header/headerLogo'
 import { Header } from './components/Header'
 
 import '../src/styles/app.css'
+import { MainVisual } from './components/MainVisual'
+import { mainImage } from './data/mainImage/mainImage'
 
 export const App: React.FC = () => {
   return (
     <div className='page-wrapper'>
       <Header menu={headerMenu} logo={headerLogo}/>
+      <main>
+        <MainVisual mainImage={mainImage}/>
+      </main>
     </div>
   )
 }
